a00c933607815fb7e1e71140287432b753cf28c6
kkflt <112418704+kkflt@users.noreply.github.com>
2025-05-10 14:12:59 +0200

    Update themes.nix

--- pkgs/themes.nix
+++ pkgs/themes.nix
@@ -86,10 +86,6 @@
         }
       ];
       patches = {
-        "xpui.js_find_8008" = ",(\\w+=)32,";
-        "xpui.js_repl_8008" = ",\${1}28,";
-      };
-      patches = {
         "xpui.js_find_8008" = ",(\\w+=)32";
         "xpui.js_repl_8008" = ",\${1}56";
       };
